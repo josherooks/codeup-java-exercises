2a6f8140c8454f798330715184239218d452b10d
Joshua Rooks <joshua.e.rooks@gmail.com>
2022-12-08 14:06:47 -0600

    finished hellow world java exercise

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+    }
+
+    //main method shortcut
+    //main(tab)
+    //psvm(tab/enter)
+
+    //System.out.println shortcut
+    //sout
+
+
+}
